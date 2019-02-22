880f67b1f65d20d59cb1bcda22c2d22eb05cc9e9
Linda Chang <lindachang186@gmail.com>
2019-02-22 04:21:24 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
+
+  attr_accessor :name, :email, :password
+
+  def intialize(name:, email:, password:)
+    @name = name
+    @email = email
+    @password = password
+  end
+
+
 end
